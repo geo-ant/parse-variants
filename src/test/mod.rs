33dd549b8d03255bdf948e89e8a1e56bd74680c3
geo-ant <54497890+geo-ant@users.noreply.github.com>
2021-06-08 23:04:29 +0200

    ok I know how to test this now

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,10 +1,11 @@
 use syn::Ident;
-use syn::LitInt;
+use syn::{LitInt,LitStr};
 
 use super::Parse;
 
 use assert2::let_assert;
 use assert2::check;
+use assert_matches::assert_matches;
 
 #[derive(Parse)]
 enum SimpleVariant {
@@ -28,10 +29,10 @@ enum ComplicatedVariants {
 
 #[test]
 fn test_testy_test() {
-    let variant = syn::parse_str::<SimpleVariant>("1,2").unwrap();
+    let variant = syn::parse_str::<SimpleVariant>("1,1239874").unwrap();
 
     let_assert!(SimpleVariant::IntCommaInt(int1,comma,int2) = variant);
-
-
-
+    check!(int1 == syn::parse_str::<LitInt>("1").unwrap());
+    check!(comma == syn::parse_str::<syn::token::Comma>(",").unwrap());
+    check!(int2 == syn::parse_str::<LitInt>("1239874").unwrap());
 }
