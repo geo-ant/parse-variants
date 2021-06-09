c4b07545248d0761b11d4a82f1cc0de39ed9a7f6
geo-ant <54497890+geo-ant@users.noreply.github.com>
2021-06-09 22:32:44 +0200

    reorder docs

--- src/lib.rs
+++ src/lib.rs
@@ -29,77 +29,83 @@
 //! # Ok(())
 //! # }
 //! ```
-//! Or in any other context where we wish to parse this type.
+//! Or in any other context where we wish to parse this type. The custom derive macro can be used on
+//! much more general `enum` types, enabling pretty powerful parsing of variants.
 //!
-//! # Usage
+//! [See the macro documentation for more use cases and some caveats](self::Parse).
 //!
-//! **Attention** This crate requires that you have the [syn crate](https://crates.io/crates/syn) as a dependency
-//! and that you have not renamed it.
-//!
-//! ## General
-//! * The custom derive can be applied to *enumerations*, which may contain struct like or
-//! tuple like variants. Each variant may contain one or multiple fields.
-//! * Every contained field must implement the `syn::parse::Parse` trait.
-//! * Member fields for each variants are parsed in order of definition.
-//! * The first variant (in order of declaration) that is successfully parsed from the input will
-//! be returned. The input `ParseBuffer` is advanced accordingly.
-//! * If no variant can be successfully parsed from the given input, a descriptive compile error
-//! is returned.
-//!
-//! ## Caveats
-//! The enum variants are parsed top to bottom. That means the first variant that can successfully
-//! parsed will be returned. That means the order matters if one variant includes other variants.
-//! Take this for example:
-//! ```
-//! // WRONG: this can never return the Identifier variant
-//! #[derive(parse_variants::Parse)]
-//! enum Number {
-//!     Expression(syn::Expr),
-//!     Identifier(syn::Ident)
-//! }
-//! ```
-//! Since identifiers can be parsed as expressions, the `Expression` variant will always be chosen,
-//! even if the given tokens could also have been parsed as an identifier.
-//!
-//! ```
-//! // CORRECT: the most general pattern comes last
-//! #[derive(parse_variants::Parse)]
-//! enum Number {
-//!     Identifier(syn::Ident),
-//!     Expression(syn::Expr)
-//! }
-//! ```
-//! This is why we have to pay attention to ordering the variants from least general to most general.
-//!
-//! ## Restrictions
-//! The enumeration cannot contain unit variants (i.e. without member fields), because there is no
-//! useful way to parse them.
-//!
-//! ## Example
-//! It is possible to write pretty complex parsers for variants. See this very silly example:
-//!
-//! ```
-//! use syn::Ident;
-//! use syn::Expr;
-//! # use assert2::let_assert;
-//!
-//! mod kw {
-//!     syn::custom_keyword!(meters);
-//! }
-//!
-//! #[derive(parse_variants::Parse)]
-//! enum SillyEnum {
-//!     ExpressionInMeters {
-//!         first: syn::Expr,
-//!         _meters: kw::meters,
-//!     },
-//!     IdentPlusPlus(Ident, syn::Token![+], syn::Token![+]),
-//! }
-//! # let_assert!(Ok(SillyEnum::ExpressionInMeters{..}) = syn::parse_str::<SillyEnum>("16 + 12*length meters"));
-//! # let_assert!(Ok(SillyEnum::IdentPlusPlus(_,_,_)) = syn::parse_str::<SillyEnum>("C++"));
-//! ```
-//! This will e.g. parse the tokens `16 + 12*length meters` as the first and `C++` as the second variant.
 
+
+/// A derive macro that allows us to parse a variant of an enumeration.
+///
+/// # Usage
+///
+/// **Attention** This crate requires that you have the [syn crate](https://crates.io/crates/syn) as a dependency
+/// and that you have not renamed it.
+///
+/// ## General
+/// * The custom derive can be applied to *enumerations*, which may contain struct like or
+/// tuple like variants. Each variant may contain one or multiple fields.
+/// * Every contained field must implement the `syn::parse::Parse` trait.
+/// * Member fields for each variants are parsed in order of definition.
+/// * The first variant (in order of declaration) that is successfully parsed from the input will
+/// be returned. The input `ParseBuffer` is advanced accordingly.
+/// * If no variant can be successfully parsed from the given input, a descriptive compile error
+/// is returned.
+///
+/// ## Caveats
+/// The enum variants are parsed top to bottom. That means the first variant that can successfully
+/// parsed will be returned. That means the order matters if one variant includes other variants.
+/// Take this for example:
+/// ```
+/// // WRONG: this can never return the Identifier variant
+/// #[derive(parse_variants::Parse)]
+/// enum Number {
+///     Expression(syn::Expr),
+///     Identifier(syn::Ident)
+/// }
+/// ```
+/// Since identifiers can be parsed as expressions, the `Expression` variant will always be chosen,
+/// even if the given tokens could also have been parsed as an identifier.
+///
+/// ```
+/// // CORRECT: the most general pattern comes last
+/// #[derive(parse_variants::Parse)]
+/// enum Number {
+///     Identifier(syn::Ident),
+///     Expression(syn::Expr)
+/// }
+/// ```
+/// This is why we have to pay attention to ordering the variants from least general to most general.
+///
+/// ## Restrictions
+/// The enumeration cannot contain unit variants (i.e. without member fields), because there is no
+/// useful way to parse them.
+///
+/// ## Example
+/// It is possible to write pretty complex parsers for variants. See this very silly example:
+///
+/// ```
+/// use syn::Ident;
+/// use syn::Expr;
+/// # use assert2::let_assert;
+///
+/// mod kw {
+///     syn::custom_keyword!(meters);
+/// }
+///
+/// #[derive(parse_variants::Parse)]
+/// enum SillyEnum {
+///     ExpressionInMeters {
+///         first: syn::Expr,
+///         _meters: kw::meters,
+///     },
+///     IdentPlusPlus(Ident, syn::Token![+], syn::Token![+]),
+/// }
+/// # let_assert!(Ok(SillyEnum::ExpressionInMeters{..}) = syn::parse_str::<SillyEnum>("16 + 12*length meters"));
+/// # let_assert!(Ok(SillyEnum::IdentPlusPlus(_,_,_)) = syn::parse_str::<SillyEnum>("C++"));
+/// ```
+/// This will e.g. parse the tokens `16 + 12*length meters` as the first and `C++` as the second variant.
 pub use parse_variants_derive::Parse;
 
 #[cfg(test)]
